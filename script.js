7832080e54e6687b3b02ad2510c8f83e4737466e
pjblitz86 <paulius.juknis@gmail.com>
2021-01-26 16:46:03 +0200

    checkLength

--- script.js
+++ script.js
@@ -20,6 +20,22 @@ function getFieldName(input) {
   return input.id.charAt(0).toUpperCase() + input.id.slice(1);
 }
 
+function checkLength(input, min, max) {
+  if (input.value.length < min) {
+    showError(
+      input,
+      `${getFieldName(input)} must be at least ${min} characters`
+    );
+  } else if (input.value.length > max) {
+    showError(
+      input,
+      `${getFieldName(input)} must be at less than ${max} characters`
+    );
+  } else {
+    showSuccess(input);
+  }
+}
+
 function checkRequired(inputArr) {
   inputArr.forEach(function (input) {
     if (input.value.trim() === '') {
@@ -35,6 +51,8 @@ form.addEventListener('submit', function (e) {
   e.preventDefault();
 
   checkRequired([username, email, password, password2]);
+  checkLength(username, 3, 15);
+  checkLength(password, 6, 25);
 });
 
 function isValidEmail(email) {
